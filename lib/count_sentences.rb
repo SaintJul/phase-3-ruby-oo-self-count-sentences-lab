5c96f2b6e2abc45779cf4c58c38fdca467eaa846
Julius.M <favor.julius@gmail.com>
2021-11-19 16:07:33 -0500

    commit tree 718d5ce2daf0dba846c6d786d21e28478ae54a20

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -2,16 +2,23 @@ require 'pry'
 
 class String
 
+  # class String
+  #   def report_on_self
+  #     "Self is: #{self}"
+  #   end
+  # end
+  
+
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
